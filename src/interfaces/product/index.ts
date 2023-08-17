8fa265b8a43956b9e4509f6860caa6e667469956
galenomoon <galeno.dev@gmail.com>
2023-08-17 19:18:01 -0300

    model category created + prisma setup + scheme created + interfaces

---
+++ src/interfaces/product/index.ts
@@ -0,0 +1,10 @@
+
+
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  images: string[];
+  category_id: number;
+}
---
+++ src/models/categoryModel.ts
@@ -0,0 +1,33 @@
+import { Category } from "../interfaces/category";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default class CategoryModel {
+  async getAll() {
+    const categories = await prisma.category.findMany();
+    return categories;
+  }
+
+  async getById(id: number) {
+    const category = await prisma.category.findUnique({
+      where: { id },
+    });
+    return category;
+  }
+
+  async create({ name }: Category) {
+    const category = await prisma.category.create({
+      data: { name }
+    })
+    return category
+  }
+
+  async update(id: number, { name }: Category) {
+    const category = await prisma.category.update({
+      where: { id },
+      data: { name }
+    })
+    return category
+  }
+}
