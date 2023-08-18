76e6c4406353e3a9bebb6c057511e794a66c3082
galenomoon <galeno.dev@gmail.com>
2023-08-18 14:33:45 -0300

    error handler created + helpers

--- src/useCases/productUseCase.ts
+++ src/useCases/productUseCase.ts
@@ -1,37 +1,50 @@
 import ProductModel from '../models/productModel';
-import { Product } from '../interfaces/product';
 import CategoryModel from '../models/categoryModel';
+import { Product } from '../interfaces/product';
+import { AppError } from '../errors/appError';
+import { hasAllRequiredKeys } from '../errors/hasAllRequiredKeys';
 
 const productModel = new ProductModel()
 const categoryModel = new CategoryModel()
 
 export default class ProductUseCase {
+  
   async getAll() {
     return await productModel.getAll()
   }
 
   async getById(id: number) {
     const product = await productModel.getById(id)
-    if (!product) throw new Error("Product not found")
+    if (!product) throw new AppError("Product not found", 404)
     return product
   }
 
   async create({ name, description, price, images, category_id }: Product) {
-    if (!name) throw new Error("name is required")
-    if (!description) throw new Error("description is required")
-    if (!price) throw new Error("price is required")
+   
+    hasAllRequiredKeys({ name, description, price, category_id })
 
-    if (!Array.isArray(images)) throw new Error("images must to be an array")
-    if (!category_id) throw new Error("category_id is required")
+    if (!Array.isArray(images)) {
+      throw new AppError("images must be an array");
+    }
 
     const is_category_id_valid = await categoryModel.getById(category_id)
-    if (!is_category_id_valid) throw new Error("Category not found")
+
+    if (!is_category_id_valid) {
+      throw new AppError("Category not found", 404)
+    }
 
     return await productModel.create({ name, description, price, images, category_id })
   }
 
   async update(id: number, { name, description, price, images, category_id }: Product) {
-    if (!name) throw new Error("Name is required")
+    
+    hasAllRequiredKeys({ name, description, price, category_id })
+
+    const is_category_id_valid = await categoryModel.getById(category_id)
+
+    if (!is_category_id_valid) {
+      throw new AppError("Category not found", 404)
+    }
 
     return await productModel.update(id, { name, description, price, images, category_id })
   }
