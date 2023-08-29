7f262e0ed5c8d075385293d286d08e42e83a8d94
galenomoon <galeno.dev@gmail.com>
2023-08-29 07:21:28 -0300

    JWT authentication 100% done and working 🥳

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
-import categoriesRoutes from './categories.routes'
+import authRoutes from './auth.routes'
 import productsRoutes from './products.routes'
+import categoriesRoutes from './categories.routes'
+import { authenticateToken } from '../middlewares/authMiddleware'
 
 const routes = Router()
 
-routes.use('/products', productsRoutes)
-routes.use('/categories', categoriesRoutes)
+routes.use('/auth', authRoutes)
+routes.use('/products', authenticateToken, productsRoutes)
+routes.use('/categories', authenticateToken, categoriesRoutes)
 
 routes.get('/', (_, res) => res.status(200).json({ message: 'OK!' }))
 
